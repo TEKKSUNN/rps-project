cd89d5cda6b7d231011653e94002c8e6f0faf175
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-07-06 18:25:15 +0800

    Add code to script.js

---
+++ script.js
@@ -0,0 +1,90 @@
+/* TODO:
+    - take player choice
+    - take computer choice
+    - process and output result
+*/
+const choices = ["rock", "paper", "scissors"];
+
+function playRPS()
+{
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+}
+
+function getHumanChoice()
+{
+    while (true) {
+        let humanChoice = window.prompt("Rock, paper, or scissors? ");
+        humanChoice = humanChoice.toLowerCase();
+        if (choices.includes(humanChoice))
+        {
+            return humanChoice;
+        }
+        else
+        {
+            continue;
+        }
+    }
+}
+
+function getComputerChoice()
+{
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(humanChoice, computerChoice)
+{
+    let showMoves = () => console.log(`You played ${humanChoice}, computer played ${computerChoice}`);
+    let showWin = () => {
+        console.log("WIN!");
+        showMoves();
+    }
+    let showLose = () => {
+        console.log("LOSE!");
+        showMoves();
+    }
+    let showDraw = () => {
+        console.log("DRAW!");
+        showMoves();
+    }
+    if (humanChoice === computerChoice)
+    {
+        showDraw();
+        return;
+    }
+    switch (humanChoice)
+    {
+        case 'rock':
+            if (computerChoice == "paper")
+            {
+                showLose();
+                return;
+            }
+            else {
+                showWin();
+                return;
+            }
+        case 'paper':
+            if (computerChoice == "scissors")
+            {
+                showLose();
+                return;
+            }
+            else {
+                showWin();
+                return;
+            }
+        case 'scissors':
+            if (computerChoice == "rock")
+            {
+                showLose();
+                return;
+            }
+            else {
+                showWin();
+                return;
+            }
+    }
+}
+
